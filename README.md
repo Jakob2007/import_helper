daed47024cef5d2db4d03a1b4817fc480ac84fce
Jakob2007 <jakob.lordcraft@outlook.de>
2023-08-27 15:06:06 +0200

    readme update

--- README.md
+++ README.md
@@ -4,23 +4,35 @@ With this module you can install packages right as you import them
 This module is primarily if you want to send your script to someone who isnt familiar with pip like clients or friends.
 
 ## Usage
-selenium is used as an example here
+
+To use this project directly in your code:
+```
+import urllib.request
+url = "https://raw.githubusercontent.com/Jakob2007/import_helper/master/import_helper.py"
+response = urllib.request.urlopen(url)
+module_code = response.read().decode("utf-8")
+exec(module_code)
+```
+
+(selenium is used as an example here)
 
 ### Usage 1
-
-> selenium = ih.imp("selenium")
-> By = ih.imp_sub_lib(("selenium", "webdriver", "common", "by")).By
-> webdriver = ih.imp_sub_lib(("selenium", "webdriver"))
+```
+selenium = ih.imp("selenium")
+By = ih.imp_sub_lib(("selenium", "webdriver", "common", "by")).By
+webdriver = ih.imp_sub_lib(("selenium", "webdriver"))
+```
 
 ### Usage 2
-
-> try:
->     import selenium
->     from selenium import webdriver
->     from selenium.webdriver.common.by import By
->   except ImportError as e:
->     print(f"installing {e.name}")
->     ih.install(e.name)
->   else:
->     print("import successful")
->     break
+```
+try:
+    import selenium
+    from selenium import webdriver
+    from selenium.webdriver.common.by import By
+  except ImportError as e:
+    print(f"installing {e.name}")
+    ih.install(e.name)
+  else:
+    print("import successful")
+    break
+```
--- import_helper.py
+++ import_helper.py
@@ -2,68 +2,82 @@
 import subprocess
 import platform
 
-# install library
-def install(name: str, logging: bool) -> None:
-	pl = platform.platform().lower()
-	if "windows" in pl:
-		command = ("pip", "install", name)
-	elif "macos" in pl or "linux" in pl:
-		py_version = '.'.join(platform.python_version().split('.')[:2])
-		command = (f"pip{py_version}", "install", name)
-	else:
-		raise Exception(f"Unsupported System: {pl}")
-	
-	result = subprocess.run(command, capture_output=True)
-
-	if result.returncode != 0:
-		raise Exception(
-			f"""
-Could not install library '{name}' exit status: {result.returncode}
-{result.stdout}
-{result.stderr}
-"""
-		)
-
-	if logging:
-		print(result.stdout)
-
-# installs the library using pip if neccessary and returns it
-def imp(lib_name: str, logging=0) -> None:
-	try: 
-		if logging >= 1: print(f"start importing {lib_name}")
-		lib = __import__(lib_name)
-	except ImportError as e:
-		if logging >= 1: print(f"failed; installing {e.name}")
-		install(e.name, logging=(logging==2))
+class Helper:
+	# install library
+	def install(name: str, logging=False) -> None:
+		pl = platform.platform().lower()
+		if "windows" in pl:
+			command = ("pip", "install", name)
+		elif "macos" in pl or "linux" in pl:
+			py_version = '.'.join(platform.python_version().split('.')[:2])
+			command = (f"pip{py_version}", "install", name)
+		else:
+			raise Exception(f"Unsupported System: {pl}")
 		
-		if logging >= 1: print("retry importing")
-		lib = __import__(lib_name)
+		result = subprocess.run(command, capture_output=True)
 
-	if logging == 1: print("done")
+		if result.returncode != 0:
+			raise Exception(
+				f"""
+	Could not install library '{name}' exit status: {result.returncode}
+	{result.stdout}
+	{result.stderr}
+	"""
+			)
 
-	return lib
+		if logging:
+			print(result.stdout)
 
-# installs the library using pip if neccessary and returns the lowest subclass e.g. imp_sub_lib(("selenium", "webdriver", "common", "by")) -> by
-def imp_sub_lib(lib_path: tuple, logging=0) -> object:
-	imp(lib_path[0], logging)
+	# installs the library using pip if neccessary and returns it
+	def imp(lib_name: str, logging=0) -> None:
+		try: 
+			if logging >= 1: print(f"start importing {lib_name}")
+			lib = __import__(lib_name)
+		except ImportError as e:
+			if logging >= 1: print(f"failed; installing {e.name}")
+			Helper.install(e.name, logging=(logging==2))
+			
+			if logging >= 1: print("retry importing")
+			lib = __import__(lib_name)
 
-	node = __import__('.'.join(lib_path))
+		if logging == 1: print("done")
 
-	for node_name in lib_path[1:]:
-		node = getattr(node, node_name)
+		return lib
 
-	return node
+	# installs the library using pip if neccessary and returns the lowest subclass e.g. imp_sub_lib(("selenium", "webdriver", "common", "by")) -> by
+	def imp_sub_lib(lib_path: tuple, logging=0) -> object:
+		Helper.imp(lib_path[0], logging)
+
+		node = __import__('.'.join(lib_path))
+
+		for node_name in lib_path[1:]:
+			node = getattr(node, node_name)
+
+		return node
 
 if __name__ == "__main__":
-	print("Thanks for using my Script\n - Jakob Sauer \033[4mFiverr.com/jakob_2\033[0m")
+	print("Thanks for supporting my work\n - Jakob Sauer \033[4mFiverr.com/jakob_2\033[0m")
 	
-	# test:
-	if True:
-		try:
-			from selenium.webdriver.common.by import By
-		except ImportError as e:
-			print("befor automatic install:")
-			print(e)
+	# test 1:
+	if False:
+		selenium = imp("selenium")
 		By = imp_sub_lib(("selenium", "webdriver", "common", "by")).By
+		webdriver = imp_sub_lib(("selenium", "webdriver"))
+		print("test successful")
+
+	# test 2:
+	if False:
+		while True:
+			try:
+				from selenium import webdriver
+				from selenium.webdriver.common.by import By
+				# import other stuff if you want to
+			except ImportError as e:
+				print(f"installing {e.name}")
+				install(e.name)
+			else:
+				print("import successful")
+				break
+
 		print(By.XPATH)
 		print("test successful")
